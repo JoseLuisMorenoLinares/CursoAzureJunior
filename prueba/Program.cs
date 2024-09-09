fedd2296d8dcd934a567606c829e44246ddf3f01
JoseLuisMorenoLinares <jose_moreno_linares@hotmail.com>
2024-09-09 11:49:38 +0000

    crear proyecto

---
+++ prueba/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Hola, Mundo!");
