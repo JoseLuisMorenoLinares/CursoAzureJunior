13c5cc765cb980163250e8b726225e79a970df25
JoseLuisMorenoLinares <jose_moreno_linares@hotmaiol.com>
2024-09-11 11:56:16 +0200

    Primeros ejercicios (variables) y primer desafio

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,4 +1,70 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.BackgroundColor = ConsoleColor.Cyan;
-Console.WriteLine("Hola, Mundo!");
+
+
+/*
+Console.WriteLine("Congratulations!");
+Console.Write("");
+Console.WriteLine("You wrote your firts lines of code.");
+*/
+
+
+/*
+string firstName;
+
+firstName = "Bob";
+Console.WriteLine(firstName);
+
+firstName = "Liem";
+Console.WriteLine(firstName);
+
+firstName = "Isabella";
+Console.WriteLine(firstName);
+
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+*/
+
+
+/*
+string firstName;
+Console.WriteLine(firstName);
+*/
+
+
+/*
+var message = "Hello word!";
+Console.WriteLine(message);
+*/
+
+
+/*
+var message = "Hello word!";
+message = 10;
+*/
+
+/*
+var message;
+*/
+
+
+/*
+int numero = 10;
+var numeroGrande = numero;
+*/
+
+
+/*
+int numero = 10;
+double numeroGrande = numero;
+*/
+
+/*
+//Desafío: Mostrar valores literales y variables
+string nombre = "Bob";
+int numeroMensajes = 3;
+double temperatura = 34.4;
+
+Console.WriteLine("Hello, " + nombre + "! You have " + numeroMensajes + " messages in you inbox. The temperature is " + temperatura + " celsius."); //Hello, Bob! You have 3 messages in your inbox. The temperature is 34.4 celsius.
+*/
+
+Console.WriteLine("c");
