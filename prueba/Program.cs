9dafda4c990a135db032a9d5a93457e73cd63413
JoseLuisMorenoLinares <jose_moreno_linares@hotmaiol.com>
2024-09-11 13:44:43 +0200

    ejercicios segundo dia

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -67,4 +67,40 @@ double temperatura = 34.4;
 Console.WriteLine("Hello, " + nombre + "! You have " + numeroMensajes + " messages in you inbox. The temperature is " + temperatura + " celsius."); //Hello, Bob! You have 3 messages in your inbox. The temperature is 34.4 celsius.
 */
 
-Console.WriteLine("c");
+
+/*
+using System.Text;
+
+Console.WriteLine("su nombre:\"pepe\" y apellidos");
+Console.WriteLine("su nombre:\tpepe\n y apellidos");
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("\u3053\u3093\u3068\u3061\u30536F World!");
+*/
+
+
+/*
+Console.Clear();
+string greeting = "Hola";
+string firstName = "Pepe";
+
+string message = $"{greeting} {firstName}!";
+
+Console.WriteLine(greeting + " " + firstName + "!");
+Console.WriteLine(message);
+*/
+
+/*
+int version = 11;
+string updateText = "Update to Windows";
+string message = $"{updateText} {version}";
+Console.WriteLine(message);
+ message = $"{updateText} {version}!";
+Console.WriteLine(message);
+*/
+
+string projectName = "First-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
+
+var path = $@"C:\Output\{projectName}\Data";
+Console.WriteLine(path);
