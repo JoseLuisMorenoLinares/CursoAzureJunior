51ab9662617796fc53a6b6b9a18b0deb5a5aee66
JoseLuisMorenoLinares <jose_moreno_linares@hotmaiol.com>
2024-09-12 13:51:25 +0200

    final calculo de notas medias

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -208,7 +208,7 @@ Console.WriteLine("Fourth: " + (++value));
 //Desafío: Cálculo de los grados Celsius según la temperatura actual en grados Fahrenheit
 //Para convertir temperaturas de grados Fahrenheit a Celsius, primero reste 32 y luego multiplique por cinco novenos (5/9).
 
-
+/*
 int fahrenheit = 94;
 
 Console.Clear();
@@ -221,3 +221,46 @@ Console.WriteLine("The temperature is " + celsius + " Celsius.");
 Console.WriteLine("The temperature is " + Math.Round(celsius, 1) + " Celsius.");
 Console.WriteLine("The temperature is " + celsius.ToString("##.#") + " Celsius.");
 Console.WriteLine("The temperature is " + decimal.Round(celsius, 1) + " Celsius.");
+*/
+
+// initialize variables - graded assignments 
+int currentAssignments = 5;
+
+int sophia1 = 93;
+int sophia2 = 87;
+int sophia3 = 98;
+int sophia4 = 95;
+int sophia5 = 100;
+int[] sophia = {93, 87, 98, 95, 100};
+
+int nicolas1 = 80;
+int nicolas2 = 83;
+int nicolas3 = 82;
+int nicolas4 = 88;
+int nicolas5 = 85;
+
+int zahirah1 = 84;
+int zahirah2 = 96;
+int zahirah3 = 73;
+int zahirah4 = 85;
+int zahirah5 = 79;
+
+int jeong1 = 90;
+int jeong2 = 92;
+int jeong3 = 98;
+int jeong4 = 100;
+int jeong5 = 97;
+
+var mediaSophia = ((sophia1 + sophia2 + sophia3 + sophia4 + sophia5) / (decimal)currentAssignments);
+var mediaNicolas = ((nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5) / (decimal)currentAssignments);
+var mediaZahirah = ((zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5) / (decimal)currentAssignments);
+var mediaJeong = ((jeong1 + jeong2 + jeong3 + jeong4 + jeong5) / (decimal)currentAssignments);
+
+Console.Clear();
+Console.WriteLine("Student\tGrade");
+Console.WriteLine("Sophia\t" + Math.Round(mediaSophia, 1) + "\t" + (mediaSophia >= 90 ? "A" : "B"));
+Console.WriteLine("Nicolas\t" + Math.Round(mediaNicolas, 1) + "\t" + (mediaNicolas >= 90 ? "A" : "B"));
+Console.WriteLine("Zahirah\t" + Math.Round(mediaZahirah, 1) + "\t" + (mediaZahirah >= 90 ? "A" : "B"));
+Console.WriteLine("Jeong\t" + Math.Round(mediaJeong, 1) + "\t" + (mediaJeong >= 90 ? "A" : "B"));
+
+
