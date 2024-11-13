0889055b54fec7bb2bfdfb018442ff2129b0de3c
joseluismorenolinares <jose_moreno_linares@hotmail.com>
2024-11-13 14:12:39 +0100

    ultima subida

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -935,3 +935,28 @@ for(int i = 0 ; i < values.Length ; i++)
 Console.WriteLine($"Message: {mensaje}");
 Console.WriteLine($"Total: {suma}");
 */
+/*
+if (ValidateLength() && ValidateZeroes() && ValidateRange()) 
+{
+    Console.WriteLine($"ip is a valid IPv4 address");
+} 
+else 
+{
+    Console.WriteLine($"ip is an invalid IPv4 address");
+}*/
+
+/*
+Random random = new Random();
+int luck = random.Next(100);
+
+string[] text = {"You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to"};
+string[] good = {"look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!"};
+string[] bad = {"fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life."};
+string[] neutral = {"appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature."};
+
+Console.WriteLine("A fortune teller whispers the following words:");
+string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
+for (int i = 0; i < 4; i++) 
+{
+    Console.Write($"{text[i]} {fortune[i]} ");
+}*/
